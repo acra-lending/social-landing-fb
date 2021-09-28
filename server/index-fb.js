85e2911ab745f1378f3ebff3930f6472a377a5a9
Chris <chrisliu0416@gmail.com>
2021-09-28 11:17:12 -0700

    Convert FormData to URLencoded

--- server/index-fb.js
+++ server/index-fb.js
@@ -9,13 +9,18 @@ app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 
 app.post('/send', async (req, res) => {
+  const data = qs.parse(req.body.formData);
+  const entries = Object.entries(data)
+  const asString = entries
+  .map(x => `${encodeURIComponent(x[0])}=${encodeURIComponent(x[1])}`)
+  .join('&');
 
     const options = {
             method: 'POST',
             headers: { 
               'Content-Type': 'application/x-www-form-urlencoded',
             },
-            data: qs.stringify(req.body),
+            data: asString,
             url: 'https://secure.velocify.com/Import.aspx?Provider=CSCWebLeadRetailPages&Client=31215&CampaignId=59'
           }
     try {
